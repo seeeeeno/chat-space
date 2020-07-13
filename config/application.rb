0a2afe059af04d12831b583c3461a23d2e3bcf0c
seeeeeno <ittdhitfhhj864269@gmail.com>
2020-07-13 18:15:34 +0900

    front-view

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,10 @@ module ChatSpace
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.generators do |g|
-      g.stylesheets fales
-      g.javascripts fales
-      g.helper fales
-      g.test_framework fales
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
     end
 
     # Settings in config/environments/* take precedence over those specified here.
